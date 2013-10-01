25cbe2394ffba18317247305cafb417227b20ca0
Justin Campbell <justin@justincampbell.me>
2013-09-30 21:39:30 -0400

    HTML skeleton

--- lib/whipped-cream/server.rb
+++ lib/whipped-cream/server.rb
@@ -55,8 +55,22 @@ module WhippedCream
     # A Sinatra application skeleton that is used to build up the web server
     # for this plugin.
     class Web < Sinatra::Application
+      get '/' do
+        erb :index
+      end
+
+      private
+
+      def controls
+        runner.plugin.controls
+      end
+
       def runner
         Runner.instance
+      end
+
+      def title
+        runner.name
       end
     end
   end
